24671cc8a2fd9fe6cfeec74c9ac751c61d39cc3b
deadw1nter <65445419+deadw1nter@users.noreply.github.com>
2021-04-28 07:55:15 +0800

    Better name changer

--- name.lua
+++ name.lua
@@ -4,35 +4,60 @@ local ISteamFriends = steamworks.ISteamFriends
 
 stealname = ui.reference("Misc", "Miscellaneous", "Steal player name")
 
-name = cvar.name
+stole_player_name = false
 
-client.set_cvar("name", "\n\xAD\xAD")
+function paint()
+    local local_player = entity.get_local_player()
 
-local function on_paint()
-        local local_player = entity.get_local_player()
-    
-        if local_player ~= nil then
-                local name = entity.get_player_name(local_player)
-                if name ~= nil then 
-                        renderer.text(1, 10, 255, 255, 255, 255, "+b", 0, "name: '"..name.."'")
-                end
+    if local_player ~= nil then
+        local name = entity.get_player_name(local_player)
+        if name ~= nil then
+            renderer.text(1, 10, 255, 255, 255, 255, "+b", 0, string.format("name: \"%s\"", name))
         end
+    end
 end
 
-client.set_event_callback("paint", on_paint)
+function player_connect_full(e)
+    if client.userid_to_entindex(e.userid) == entity.get_local_player() then 
+        client.delay_call(globals.tickinterval(), function()
+            local is_warmup_period = entity.get_prop( ntity.get_game_rules(), "m_bWarmupPeriod")
+            if is_warmup_period == 0 then
+                if not stole_player_name then
+                    ui.set(stealname, true)
+                    stole_player_name = true
+                end
 
-client.set_event_callback("player_connect_full", function(e)
-        if client.userid_to_entindex(e.userid) == entity.get_local_player() then 
-                ui.set(stealname, true)
-
-                name:invoke_callback()
                 client.set_cvar("name", "\n\xAD\xAD")
                 client.delay_call(0.2, function()
-                        client.set_cvar("name", "nigger\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") 
+                        client.set_cvar("name", ISteamFriends.GetPersonaName()) 
                 end)
-        end
-end)
+            end
+        end)
+    end
+end
 
-client.set_event_callback("cs_win_panel_match", function(e)
-        client.set_cvar("name", "nigger\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") 
-end)
+function round_prestart(e)
+    client.delay_call(globals.tickinterval(), function()
+        local is_warmup_period = entity.get_prop(entity.get_game_rules(), "m_bWarmupPeriod")
+        if is_warmup_period == 0 then
+            if not stole_player_name then
+                ui.set(stealname, true)
+                stole_player_name = true
+            end
+
+            client.set_cvar("name", "\n\xAD\xAD")
+            client.delay_call(0.2, function()
+                    client.set_cvar("name", ISteamFriends.GetPersonaName()) 
+            end)
+        end
+    end)
+end
+
+function cs_win_panel_match()
+    client.set_cvar("name", "ez game\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") 
+end
+
+client.set_event_callback("paint", paint)
+client.set_event_callback("player_connect_full", player_connect_full)
+client.set_event_callback("round_prestart", round_prestart)
+client.set_event_callback("cs_win_panel_match", cs_win_panel_match)
